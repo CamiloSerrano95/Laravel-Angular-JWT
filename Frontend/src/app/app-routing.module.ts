974693c630fa8602a7e5fd9173723f78e235f89c
Camilo Serrano <casegica@gmail.com>
2020-04-30 23:15:29 -0500

    [Frontend] add new component for user and guard

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
+    {path: 'register', component: RegisterComponent},
+    {path: 'user', component: UserComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
