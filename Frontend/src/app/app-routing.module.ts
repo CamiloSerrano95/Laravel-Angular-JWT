0391f6621633b1f2e0a77ae3357a3f6bc9bf14fc
Camilo Serrano <casegica@gmail.com>
2020-05-03 14:17:52 -0500

    [Frontend] Add new routing for module for admin

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { UserComponent } from './user/user.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'user', component: UserComponent, canActivate: [AuthGuard]}
+    {path: 'admin', canActivate: [AuthGuard], loadChildren: () => import('src/app/admin/admin.module').then(m => m.AdminModule)}
 ];
 
 @NgModule({
