b11ef53d6aa62d685fa534700758d1848763886f
Camilo Serrano <casegica@gmail.com>
2020-05-03 14:15:40 -0500

    [Frontend] Add new module for admin

---
+++ Frontend/src/app/admin/admin.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { AdminRoutingModule } from './admin-routing.module';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, UserComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
