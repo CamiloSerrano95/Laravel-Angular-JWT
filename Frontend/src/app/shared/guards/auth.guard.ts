c63f1c4446e052f06e86116c0320e27663907e6d
Camilo Serrano <casegica@gmail.com>
2020-05-03 14:14:35 -0500

    [Frontend] add guard for verify user

--- Frontend/src/app/shared/guards/auth.guard.ts
+++ Frontend/src/app/shared/guards/auth.guard.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  
+  constructor(
+    private authService: AuthService,
+    public router: Router
+  ){}
+
+  canActivate(): boolean {    
+    if(!this.authService.isAuthenticated()) {
+      this.router.navigate(['/']);
+      return false;
+    }
+
     return true;
   }
   
