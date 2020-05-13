532123c2116c1216a410bd7519d01daec650c5ae
Camilo Serrano <casegica@gmail.com>
2020-05-12 23:23:07 -0500

    [Frontend] add methods for get all, get and create a user

---
+++ Frontend/src/app/admin/user/users.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  data:any;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllUsers() {
+    const url = `${environment.apiUrl}/user/all`
+    return this.http.get(url, this.getHeader());
+  }
+
+  getUserById(id:string) {
+    const url = `${environment.apiUrl}/user/getById/${id}`
+    return this.http.get(url, this.getHeader());
+  }
+
+  newUser(data:any) {
+    const url = `${environment.apiUrl}/user/store`;
+    return this.http.post(url, data, this.getHeader());
+  }
+
+  getHeader() {
+    let user = JSON.parse(localStorage.getItem('user'));
+
+    let headers = new HttpHeaders(
+      {
+        'Authorization': `Bearer ${user.token}`
+      }
+    );
+
+    let options = { headers: headers };
+
+    return options;
+  }
+}
