3cc7687274d25848095136d3b84d8d696f18c9c2
Camilo Serrano <casegica@gmail.com>
2020-05-12 23:25:44 -0500

    [Frontend] add routes for go to create user and get user by id

--- Frontend/src/app/admin/admin-routing.module.ts
+++ Frontend/src/app/admin/admin-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { UserShowComponent } from './user/user-show/user-show.component';
+import { UserManagerComponent } from './user/user-manager/user-manager.component';
 
 const adminRoutes: Routes = [
-    {path: '', component: UserComponent}
+    {path: '', component: UserComponent},
+    {path: 'user/manager', component: UserManagerComponent},
+    {path: 'user/:id', component: UserShowComponent},
 ];
 
 @NgModule({
