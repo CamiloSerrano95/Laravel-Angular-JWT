5ba3e3d5d370ad1d40adb302d7670202e53af596
Camilo Serrano <casegica@gmail.com>
2020-05-12 23:25:08 -0500

    [Frontend] add reactive form

--- Frontend/src/app/admin/admin.module.ts
+++ Frontend/src/app/admin/admin.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { UserShowComponent } from './user/user-show/user-show.component';
+import { UserManagerComponent } from './user/user-manager/user-manager.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HeaderComponent, UserComponent],
+  declarations: [HeaderComponent, UserComponent, UserShowComponent, UserManagerComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class AdminModule { }
