b31a0f39c03eb9ae0f49aba973012b313b3dd5a1
CamiloSerrano95 <casegica@gmail.com>
2020-05-19 23:59:49 -0500

    [Backend] add rules of validation for client

---
+++ Backend/app/Http/Requests/Client/CreateClientRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Client;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateClientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'names'          => 'required|string|max:255', 
+            'identification' => 'required|string|max:12|unique:clients', 
+            'telephone'      => 'required|string|max:12', 
+            'address'        => 'required|string|max:255', 
+            'email'          => 'required|string|email|max:255|unique:clients',
+        ];
+    }
+}
